3362ba2d3217276b523c8546dc824e7b30fc94e1
Manas Asylbek <asylbekmanass@gmail.com>
2022-07-26 11:55:57 +0600

    add:Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import {BrowserRouter, Routes,Route} from "react-router-dom";
+import Main from "./Pages/Main/Main";
+import Registration from "./Pages/Registration/Registration";
+import Authorizations from "./Pages/Authorization/Authorizations";
+
 
 function App() {
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route  path="/" element={<Main/>}/>
+          <Route  path="/registration" element={<Registration/>}/>
+          <Route  path="/authorizations" element={<Authorizations/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/Pages/Authorization/Authorizations.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Authorizations = () => {
+    return (
+        <div>
+            Authorizations
+        </div>
+    );
+};
+
+export default Authorizations;
